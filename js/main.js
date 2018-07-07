afc7e21ff16c24baa995d761dc990d71a91eb69c
Tomohiro Matsuo <gacktomo@gmail.com>
2018-07-08 05:01:31 +0900

    Fix webpack config

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import * as PIXI from 'pixi.js';
 import {TweenMax} from "gsap/TweenMax";
-var Graph = require('./js/graph');
-var Network = require('./js/network');
-var UI = require('./js/UI');
+var Graph = require('./graph');
+var Network = require('./network');
+var UI = require('./UI');
 
 window.onload = function () {
   var ui = new UI();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  entry: __dirname + "/src/main.js", //ビルドするファイル
+  entry: __dirname + "/js/main.js", //ビルドするファイル
   output: {
-    path: __dirname +'/dist', //ビルドしたファイルを吐き出す場所
+    path: __dirname +'/', //ビルドしたファイルを吐き出す場所
     filename: 'bundle.js' //ビルドした後のファイル名
   },
   devServer: {
-    contentBase: __dirname + "/src",
+    contentBase: __dirname + "/",
     port: 8080
   },
   // module: {
